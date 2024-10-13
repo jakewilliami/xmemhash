d297bf324520567bc6f67744bfb6b4898eb22730
Jake Ireland <jakewilliami@icloud.com>
2024-10-13 13:47:19 +1300

    Add support for SHA1

--- src/algo.rs
+++ src/algo.rs
@@ -2,5 +2,6 @@ use clap::ValueEnum;
 
 #[derive(ValueEnum, Clone)]
 pub enum HashAlgo {
+    Sha1,
     Sha256,
 }
--- src/hash.rs
+++ src/hash.rs
@@ -1,15 +1,26 @@
 use super::algo::HashAlgo;
 use super::archive::EnclosedFile;
-use sha2::{Digest, Sha256};
+use digest::Digest;
+use sha1::Sha1;
+use sha2::Sha256;
 
-fn get_hash_from_data(data: &Vec<u8>, algo: &HashAlgo) -> String {
-    let mut hasher = match algo {
-        HashAlgo::Sha256 => Sha256::new(),
-    };
-
+// https://stackoverflow.com/q/64326373/
+fn compute_hash<D: Digest>(data: &Vec<u8>) -> String
+where
+    D::OutputSize: std::ops::Add,
+    <D::OutputSize as std::ops::Add>::Output: digest::generic_array::ArrayLength<u8>,
+{
+    let mut hasher = D::new();
     hasher.update(data);
     let hash = hasher.finalize();
     format!("{:x}", hash)
+}
+
+fn get_hash_from_data(data: &Vec<u8>, algo: &HashAlgo) -> String {
+    match algo {
+        HashAlgo::Sha1 => compute_hash::<Sha1>(data),
+        HashAlgo::Sha256 => compute_hash::<Sha256>(data),
+    }
 }
 
 pub fn get_hash_from_enclosed_file(file: &EnclosedFile, algo: &HashAlgo) -> String {
