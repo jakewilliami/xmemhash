e50586a0e0e098b14a173f6ef12b28766ed5dceb
Jake Ireland <jakewilliami@icloud.com>
2026-07-18 14:47:09 +1200

    Support 7z files whose headers are also encrypted!  v1.7.1
    I realised that we assume that we can read the 7z file header.
    However, if you create an encrypted 7z including the header:
    
        7z a -t7z <output> <input> -mhe=on -p<password>
    
    Then xmemhash would fail.  We are now more deliberate in checking
    whether we can decrypt the file before trying to read its contents.

--- src/decompress/sevenzip.rs
+++ src/decompress/sevenzip.rs
@@ -4,10 +4,7 @@
 
 use crate::archive::{ArchiveEntry, EntryData};
 use rpassword::prompt_password;
-use sevenz_rust::{
-    Error::{MaybeBadPassword, PasswordRequired},
-    Password, SevenZReader,
-};
+use sevenz_rust::{Error::PasswordRequired, Password, SevenZReader};
 use std::{
     fs::File,
     io::{Read, Seek},
@@ -15,60 +12,60 @@ use std::{
     process,
 };
 
-fn sz_archive_is_encrypted<R>(szr: &mut SevenZReader<R>) -> bool
+fn sz_archive_is_unencrypted<R>(szr: &mut SevenZReader<R>) -> bool
 where
-    R: Seek,
-    R: Read,
+    R: Seek + Read,
 {
-    // You have to try to read the file entries in order to check if you get
-    // a password required error
+    // You have to try to read the file contents within the archive before we know
+    // if the password was (maybe) incorrect
     //
-    // TODO: is there a better way to check if the file is encrypted?  E.g.
+    // TODO: is there a better way to check if the file is still encrypted?  E.g.,
     //   https://docs.rs/crate/sevenz-rust/0.6.1/source/src/decoders.rs#142
-    matches!(szr.for_each_entries(|_, _| Ok(true)), Err(PasswordRequired))
+    szr.for_each_entries(|_file, reader| {
+        let mut content = Vec::new();
+        reader.read_to_end(&mut content)?;
+        Ok(true)
+    })
+    .is_ok()
 }
 
 fn try_decrypt_from_7z_archive(path: &String) -> SevenZReader<File> {
     let password = prompt_password("Enter password: ").unwrap();
     let password = Password::from(password.as_str());
 
-    SevenZReader::open(path, password).unwrap()
-}
-
-fn sz_archive_is_unencrypted<R>(szr: &mut SevenZReader<R>) -> bool
-where
-    R: Seek,
-    R: Read,
-{
-    // You have to try to read the file contents within the archive before we know
-    // if the password was (maybe) incorrect
-    //
-    // TODO: is there a better way to check if the file is still encrypted?
-    !matches!(
-        szr.for_each_entries(|_file, reader| {
-            let mut content = Vec::new();
-            let _ = reader.read_to_end(&mut content)?;
-            Ok(true)
-        }),
-        Err(MaybeBadPassword(..))
-    )
-}
-
-pub fn get_files_from_7z_archive(path: &String) -> Vec<ArchiveEntry> {
-    // Specify blank password if not password protected
-    let password = Password::from("");
-    let mut szr = SevenZReader::open(path, password).unwrap();
-
-    // Request password from user if required
-    if sz_archive_is_encrypted(&mut szr) {
-        szr = try_decrypt_from_7z_archive(path);
-        if !sz_archive_is_unencrypted(&mut szr) {
-            // TODO: try three more times
-            // TODO: Note that MaybeBadPassword could also be due to corrupted 7z file
+    let mut szr = match SevenZReader::open(path, password) {
+        Ok(szr) => szr,
+        Err(_) => {
             eprintln!("[ERROR] Incorrect password");
             process::exit(1);
         }
+    };
+
+    if !sz_archive_is_unencrypted(&mut szr) {
+        eprintln!("[ERROR] Incorrect password");
+        process::exit(1);
     }
+
+    szr
+}
+
+pub fn get_files_from_7z_archive(path: &String) -> Vec<ArchiveEntry> {
+    // TODO: try three more times if password did not work?
+    // TODO: Note that MaybeBadPassword could also be due to corrupted 7z files
+    let mut szr = match SevenZReader::open(path, Password::from("")) {
+        Ok(mut szr) => {
+            if sz_archive_is_unencrypted(&mut szr) {
+                szr
+            } else {
+                try_decrypt_from_7z_archive(path)
+            }
+        }
+        Err(PasswordRequired) => try_decrypt_from_7z_archive(path),
+        Err(e) => {
+            eprintln!("[ERROR] Failed to open archive: {e}");
+            process::exit(1);
+        }
+    };
 
     let mut files = Vec::new();
 
